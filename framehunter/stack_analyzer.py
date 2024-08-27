516015bf5719c57ca058173bd593594ced5cad02
dmskhs0912 <dmskhs0912@gmail.com>
2024-08-27 20:54:44 +0900

    Update

--- framehunter/stack_analyzer.py
+++ framehunter/stack_analyzer.py
@@ -6,6 +6,7 @@
 
 from .disassembler import Disassembler
 from .elf_parser import ELFParser
+from .stack_frame import StackFrame
 
 class StackAnalyzer:
     def __init__(self, elf_file_path: str):
@@ -14,22 +15,70 @@ class StackAnalyzer:
         self.functions = self.elf_parser.get_function_symbols() # 딕셔너리 형태로 함수 이름과 시작 주소, 끝 주소를 저장
         self.machine_codes = self.disassembler.disassemble_code(self.elf_parser.get_section_data('.text'), 
                                                                 self.elf_parser.get_section_offset('.text')) # 기계어 코드를 저장
-        self.functions_boundaries = self.disassembler.find_function_boundaries() # 함수의 시작 주소와 끝 주소를 저장
+        #self.functions_boundaries = self.disassembler.find_function_boundaries() # 함수의 시작 주소와 끝 주소를 저장
 
 
-    def analyze_function_stack(self, function_name: str):
+    def analyze_function_stack(self, function_name) -> StackFrame:
         """
+        Returns the stack frame of the given function.
+
+        :param function_name: The name or offset of the function to analyze
+        """
+        stack_size = self._get_stack_size(function_name)
+        return_address_offset = 8 # Return address offset from RBP 
+        canary_offset = self._get_canary_offset(function_name)
+        stack_frame = StackFrame(stack_size, return_address_offset, canary_offset)
+        return stack_frame
+
+    def _get_stack_size(self, function_name) -> int:
+        """
+        Returns the size of the stack frame for the given function.
+
+        :param function_name: The name or offset of the function
+        :return: The size of the stack frame in bytes
+        """
+        if isinstance(function_name, str): # 해당 함수의 심볼이 존재하는 경우 함수이름으로 검색
+            if function_name in self.functions:
+                start_address, end_address = self.functions[function_name]
+            else:
+                raise ValueError(f'Function {function_name} not found.')
+            
+            function_code = [instr for instr in self.machine_codes if start_address <= instr.address < end_address]
+            for instr in function_code:
+                if instr.mnemonic == 'sub' and instr.op_str.startswith('rsp, '):
+                    try:
+                        return int(instr.op_str.split(',')[1].strip(), 16)
+                    except:
+                        raise ValueError(f'Failed to find stack size for function {function_name}')
+            raise ValueError(f'Failed to find stack size for function {function_name}')
         
+        else:
+            raise NotImplementedError('Only function names are supported for now.')
+        
+    def _get_canary_offset(self, function_name) -> int:
         """
-        pass
+        Returns the offset of the canary value in the stack frame for the given function.
 
-    def _get_machine_code(self, start_address: int, size: int) -> bytes:
-        # ELF 파일에서 기계어 코드를 읽어오는 로직을 구현
-        # 예시로 빈 바이트 배열을 반환
-        return b'\x90' * size
-
-    def _analyze_stack(self, instructions: list) -> dict:
-        stack_structure = {}
-        # 스택 구조 분석 로직을 구현
-        # 예시로 빈 딕셔너리를 반환
-        return stack_structure
+        :param function_name: The name or offset of the function
+        :return: The offset of the canary value from RBP if found, otherwise None
+        """
+        if isinstance(function_name, str): # 해당 함수의 심볼이 존재하는 경우 함수이름으로 검색
+            if function_name in self.functions:
+                start_address, end_address = self.functions[function_name]
+            else:
+                raise ValueError(f'Function {function_name} not found.')
+            
+            function_code = [instr for instr in self.machine_codes if start_address <= instr.address < end_address]
+            for i, instr in enumerate(function_code):
+                if instr.mnemonic == 'mov' and 'fs:0x28' in instr.op_str:
+                    next_instr = function_code[i+1]
+                    if next_instr.mnemonic == 'mov' and next_instr.op_str.startswith('QWORD PTR [rbp-'):
+                        try:
+                            return -int(next_instr.op_str.split('[')[1].split(']')[0].split('-')[1].strip(), 16)
+                        except:
+                            raise ValueError(f'Failed to find canary offset for function {function_name}')
+            return None
+        
+        else:
+            raise NotImplementedError('Only function names are supported for now.')
+        
--- framehunter/stack_frame.py
+++ framehunter/stack_frame.py
@@ -5,9 +5,7 @@
 # the stack contents analyzed by the stack_analyzer.
 # ---------------------------------------------------
 
-BYTE = 1
-DWORD = 4
-QWORD = 8
+from capstone import CsInsn
 
 class StackFrame:
     """
@@ -25,3 +23,24 @@ class StackFrame:
         self.return_address_offset = return_address_offset
         self.canary_offset = canary_offset
         self.local_variables = {}
+
+    def add_local_variable(self, offset, size, instructions:list[CsInsn]):
+        """
+        Adds a local variable to the stack frame.
+
+        :param offset: The offset from RBP.
+        :param size: The size of the variable in bytes.
+        :param instructions: The list of instructions that access the variable.
+        """
+        self.local_variables['rbp-%d' % offset] = (size, instructions)
+
+    def get_local_variable(self, offset):
+        """
+        Returns the size and instructions that access the variable of the local variable at the given offset.
+
+        :param offset: The offset from RBP.
+        :return: The size and instructions of the variable in bytes.
+        """
+        return self.local_variables.get('rbp-%d' % offset, None)
+    
+    
